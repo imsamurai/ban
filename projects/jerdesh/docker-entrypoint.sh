254b43c78795902377808432b8ce08a1bd293f84
Shamrai Aleksandr <aleksandr.shamrai@together.com>
2022-03-06 22:27:07 +0200

    jerdesh

---
+++ projects/jerdesh/docker-entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+tor --RunAsDaemon 1 --CookieAuthentication 0 --HashedControlPassword "" --SocksPort 0.0.0.0:9050
+
+echo "российский военній корабль, иди на хуй!"
+
+wget https://github.com/imsamurai/ban/raw/master/data/ph.txt
+wget https://raw.githubusercontent.com/Raven-SL/ru-pnames-list/master/lists/male_names_rus.txt
+
+while true; do cat "ph.txt" | shuf | sed 's/+//' | xargs -r -P 1 -L1 -d '\n' proxychains -q ./run.sh; done;
