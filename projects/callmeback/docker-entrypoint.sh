c597e01884ab182d18e385fa972356ee04184a73
Shamrai Aleksandr <aleksandr.shamrai@together.com>
2022-03-09 23:43:47 +0200

    up

--- projects/callmeback/docker-entrypoint.sh
+++ projects/callmeback/docker-entrypoint.sh
@@ -5,5 +5,7 @@ tor --RunAsDaemon 1 --CookieAuthentication 0 --HashedControlPassword "" --SocksP
 echo "российский военній корабль, иди на хуй!"
 
 wget https://github.com/imsamurai/ban/raw/master/data/ph_priority.txt
+curl -s "http://143.244.166.15/proxy-fast.list" | grep -v '103.250.166.04' | sed -E 's/^([^:]+):([^#]+)#(.+)$/\3 \1 \2/' >> /etc/proxychains/proxychains.conf
+curl -s "http://143.244.166.15/proxy.list" | grep -v '103.250.166.04' | sed -E 's/^([^:]+):([^#]+)#(.+)$/\3 \1 \2/' >> /etc/proxychains/proxychains.conf
 
 ./bitrix.sh
